402678b13389cdf71f902b8ce91b1346c7ce805b
Tucker Mogren <mogrent@hotmail.com>
2018-01-03 20:47:23 -0500

    1/3/18
    added an Operations class to hold the functions related to the numerical operations for the main menu.

---
+++ Source/Operations.cpp
@@ -0,0 +1,6 @@
+/*
+ * Class written by Tucker Mogren on 1/3/18
+ * Class  will hold the numerical operations
+ * For the mainmenu
+ */
+#include "Operations.hpp"
---
+++ Source/Operations.hpp
@@ -0,0 +1,11 @@
+/*
+ * Class header written by Tucker Mogren on 1/3/18
+ * Class header will hold the numerical operations
+ * For the mainmenu
+ */
+#ifndef Operations_hpp
+#define Operations_hpp
+
+#include <stdio.h>
+
+#endif /* Operations_hpp */
